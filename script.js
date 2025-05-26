a5ac0b1a4875eddfd249acd70142354fa9cd5476
Cauã <92538361+raulino20@users.noreply.github.com>
2025-05-26 16:59:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ let Elementos = document.getElementsByClassName('ListH')
 
 const WP = document.getElementById('Whats')
 
-WP.addEventListerner('click',function(){
+WP.addEventListener('click',function(){
     const Text = `Olá, Preciso de um Serviço seu. ` 
     const Url = `https://wa.me/<5511965404572?text=${Text}`
     window.open(Url,'_blank')    
