cb5542bbf51fb5b50e1ea467a62a88c0010caaec
Cauã <92538361+raulino20@users.noreply.github.com>
2025-06-11 17:33:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -101,6 +101,133 @@ let Elementos = document.getElementsByClassName('ListH')
           Posicionar.appendChild(NewCard)
       }
     }
+///////////////////////////////////////////////////////////////////////
+
+//////////  CONTAINER DOS CERTIFICADOS /////////////////////// 
+let Container = document.getElementById('Certificado') 
+
+document.getElementById('Open').addEventListener('change',function(){
+  let valor = this.value 
+  
+switch(valor){
+
+  case '0':
+    Container.innerText = ''
+    break
+
+  case '1':
+  Container.innerText = ''
+  let WIDT = window.matchMedia('(max-width:768px)')
+  const HTML = document.createElement('img')
+  
+  HTML.src = 'public/imagens/Certificado HTML5.jpg'
+    HTML.alt = 'IMG-HTML'
+    HTML.style.margin = 'auto auto auto auto'
+
+    HTML.style.transform = 'matrix(1, 0, 0,1, 0, 0)'
+  if(WIDT.matches){
+
+    HTML.style.width = '100%'
+    console.log('HTML5 - Mobile')
+   }else{
+    HTML.style.width = '50%'
+    console.log('HTML5 - Desktop')
+  }
+
+   Container.appendChild(HTML)
+   console.log('HTML')
+   
+  break
+    
+  case '2':
+  Container.innerText = ''
+  let WIDT2 = window.matchMedia('(max-width:768px)')
+  const CSS = document.createElement('img')
+  
+    CSS.src = 'public/imagens/Certificado CSS.jpg'
+    CSS.alt = 'IMG-CSS'
+  if(WIDT2.matches){
+    CSS.style.width = '100%'
+    console.log('CSS - Mobile')
+  }else{
+    CSS.style.width = '50%'
+    console.log('CSS - Desktop')
+  }
+    CSS.style.margin = 'auto auto auto auto'
+ 
+    Container.appendChild(CSS)
+    console.log('CSS')
+
+  break
+
+  case '3':
+  Container.innerText = ''
+  let WIDT3= window.matchMedia('(max-width:768px)')
+  const CSSM = document.createElement('img')
+
+    CSSM.src = 'public/imagens/Certificado CSS MODERNO.jpg'
+    CSSM.alt = 'IMG-CSSM'
+  if(WIDT3.matches){
+     CSSM.style.width = '100%'
+     console.log('CSS MODERNO - Mobile')
+  }else{
+     CSSM.style.width = '50%'
+     console.log('CSS MODERNO - Desktop')
+  }
+    CSSM.style.margin = 'auto auto auto auto'
+
+    Container.appendChild(CSSM)
+    console.log('CSS MODERNO')
+
+  break
+
+  case '4':
+  Container.innerText = ''
+  let WIDT4 = window.matchMedia('(max-width:768px)')
+  const BOOTSTRAP = document.createElement('img')
+
+    BOOTSTRAP.src = 'public/imagens/Certificado Bootstrap.jpg'
+    BOOTSTRAP.alt = 'IMG-BOOTSTRAP'
+  if(WIDT4.matches){
+      BOOTSTRAP.style.width = '100%'
+      console.log('BOOTSTRAP - Mobile')
+  }else{  
+      BOOTSTRAP.style.width = '50%'
+      console.log('BOOTSTRAP - Desktop')
+  }
+    BOOTSTRAP.style.margin = 'auto auto auto auto'
+
+    Container.appendChild(BOOTSTRAP)
+    console.log('BOOTSTRAP')
+
+  break
+
+  case '5':
+    Container.innerText = ''
+    let WIDT5 = window.matchMedia('(max-width:768px)')
+    const SASS = document.createElement('img')
+
+    SASS.src = 'public/imagens/Certificado SASS.jpg'
+    SASS.alt = 'IMG-SASS'
+    
+  if(WIDT5.matches){
+      SASS.style.width = '100%'
+      console.log('SASS - Mobile')
+  }else{
+      SASS.style.width = '50%'
+      console.log('SASS - Desktop')
+  }
+    SASS.style.margin = 'auto auto auto auto'
+
+    Container.appendChild(SASS)
+    console.log('SASS')
+
+  break
+
+  }
+}) 
+
+//////
 
 //FECHAR O CONTAINER APÓS O CLICK PARA VOLTAR AO HEADER (TAMANHO 768PX OU MENOS)
     let CollapseSM = document.getElementById('Aqui')
